ca8f7271c561baf8efb830abc124fda0fe0a1b1c
PiQuark6046 <piquark6046@protonmail.com>
2022-02-09 04:46:32 +0000

    Simplify SearchElementHasStyle function

--- List-KR-ScriptDev.user.js
+++ List-KR-ScriptDev.user.js
@@ -8,7 +8,7 @@
 // @downloadURL  https://github.com/List-KR/List-KR-Script/raw/master/List-KR-ScriptDev.user.js
 // @license      MPL-2.0
 //
-// @version      1.0d8
+// @version      1.0d9
 // @author       PiQuark6046 ( piquark6046@protonmail.com ) and contributors
 //
 // @match        *://namu.wiki/w/*
@@ -74,12 +74,13 @@ const LKSLib =
     },
     SearchElementHasStyle: function(ElementType, HasStyle)
     {
-        var Elements = typeof ElementType == "string" ? LKSLib.window.document.querySelectorAll(ElementType) : ElementType, ReturnArray = [];
+        var Elements = typeof ElementType == "string" ? LKSLib.window.document.querySelectorAll(ElementType) : ElementType;
         if (NodeList.prototype.isPrototypeOf(Elements))
         {
             return LKSLib.SearchElementHasStyle(Array.from(Elements), HasStyle);
         }
-        for (var i in Elements)
+        return LKSLib.CheckElementHasStyle(Elements, HasStyle);
+    },
         {
             if (Elements[i].style[HasStyle.split(LKSConstant.StyleSplitKey)[0]] == HasStyle.split(LKSConstant.StyleSplitKey[1]))
             {
