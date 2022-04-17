cee14eb97abd658ef27b09e25133c1deec925ed2
HoJeong Go <seia@outlook.kr>
2022-04-18 00:41:09 +0900

    chore: normalize the code using jsc transform

--- src/transform.ts
+++ src/transform.ts
@@ -22,6 +22,28 @@ export const typescript = async (code: string) => {
   return output;
 };
 
+export const javascript = async (code: string) => {
+  const output = await swc
+    .transform(code, {
+      sourceMaps: true,
+      isModule: true,
+      jsc: {
+        parser: {
+          syntax: 'ecmascript',
+        },
+        transform: {
+          legacyDecorator: true,
+          decoratorMetadata: true,
+        },
+        target: 'es5',
+        keepClassNames: true,
+        loose: false,
+      },
+    });
+
+  return output;
+};
+
 export const auto = async (code: string, extension: string) => {
   let result = {
     provider: 'none',
@@ -29,6 +51,14 @@ export const auto = async (code: string, extension: string) => {
   };
 
   switch (extension) {
+    case 'js': {
+      result = {
+        provider: 'javascript',
+        ...await javascript(code),
+      };
+
+      break;
+    }
     case 'ts': {
       result = {
         provider: 'typescript',
